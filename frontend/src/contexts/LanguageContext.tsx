6c20b57228829e30c9820dd0da482b70f1aa729c
Danyou7 <161011028+Danyou7@users.noreply.github.com>
2026-09-20 18:46:27 +0700

    Update LanguageContext.tsx

--- frontend/src/contexts/LanguageContext.tsx
+++ frontend/src/contexts/LanguageContext.tsx
@@ -1,5 +1,5 @@
-import React, { createContext, useState, useContext, ReactNode } from 'react';
-
+import React, { createContext, useState, useContext, type ReactNode } from 'react';'
+  
 type Language = 'id' | 'en';
 
 interface LanguageContextType {
